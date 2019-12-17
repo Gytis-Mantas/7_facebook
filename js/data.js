9431215b82ecfbe229b918ad231fa2f96becfb96
-=Gytis=- <58184903+Gytis-Mantas@users.noreply.github.com>
2019-12-17 21:18:31 +0200

    init

---
+++ js/data.js
@@ -0,0 +1,22 @@
+"use strict";
+
+const posts =[
+    { 
+        author: {
+            name: "John",
+            surname: "Dear",
+            img: './'
+            link: '#'
+        },
+        time: 1555987665,
+        content: {
+            text: '',
+            img: [  ]
+        }
+    },
+];
+
+console.log(posts)
+
+
+
