a9c60fc7db024f0d7650318f43f865e57332d6f0
Gytis-Mantas <gytizz@gmail.com>
2020-01-03 16:42:40 +0200

    data JSON fix

--- js/action.js
+++ js/action.js
@@ -1,3 +1,5 @@
 "use strict";
-
-renderFeed( posts );
+getPosts( renderFeed )
+// vietoje buvusios funkcijos "renderFeed( posts )" 
+// naudosime nauja funkcija "getPosts", 
+// kuri atsisius duomenis ir perduos juos funkcijai "renderFeed"
